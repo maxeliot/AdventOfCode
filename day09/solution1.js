0b9972f7c1be227b5355eb056d5a9962fd5e25cd
Max Hurlimann <hurlimannmax@gmail.com>
2023-12-20 12:40:40 +0100

    Automatic git push

--- day09/solution1.js
+++ day09/solution1.js
@@ -3,3 +3,30 @@ const path = require('path');
 const input = path.join(__dirname, '/input.txt');
 
 let lines = fs.readFileSync(input, "utf8").split("\n");
+
+function nextInput(numbers) {
+    let sequence = [numbers];
+    let currIndex = 0;
+    while(sequence[sequence.length-1].filter(nbr => nbr != 0).length != 0) {
+        let nextSequence = [];
+        for(let j = 0; j < sequence[currIndex].length - 1; j++) {
+            nextSequence[j] = sequence[currIndex][j+1] - sequence[currIndex][j];
+        }
+        sequence[currIndex+1] = nextSequence;
+        currIndex++;
+    }
+    sequence[sequence.length - 1].push(0);
+
+    for(let i = sequence.length - 2; i >= 0; i--) {
+        sequence[i].push(sequence[i+1].pop() + sequence[i].pop())
+    }
+
+    return sequence[0].pop();
+}
+
+let sum = 0;
+for(numbers of lines) {
+    sum += nextInput(numbers.split(" ").map(str => parseInt(str)));
+}
+
+console.log(sum);
